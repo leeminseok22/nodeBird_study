eb2796fad3617aea765ae952551ad4bf4bd0b845
leeminseok22 <cube9072@naver.com>
2023-01-31 16:50:18 +0900

    Update: cors

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -10,11 +10,11 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  }));
-
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//   }));
+app.use(cors());
 
 
 db.sequelize.sync()
